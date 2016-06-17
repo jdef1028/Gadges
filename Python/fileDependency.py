31862cf0518d05f1648e3e578b537e7acdbc2d30
Xiaolin <xiaolinli2018@u.northwestern.edu>
2016-06-17 17:15:59 -0500

    Dependecy update

--- Python/fileDependency.py
+++ Python/fileDependency.py
@@ -4,18 +4,29 @@
 import os.path
 
 class fileDependency():
-	def __init__(self, inputFile, outputFile, *proc):
+	def __init__(self, inputFile, outputFile, fun):
 		# inputFile: input File path
 		# outputFile: output File path
-		# *proc: if the outputfile is not up-to-date, the processed to be taken
+		# *proc: if the outputfile is not up-to-date, the process to be taken
+		flag = True
+		assert os.path.isfile(inputFile)
+		if not os.path.isfile(outputFile):
+			flag = False
 		
-		self.inputModTime = os.path.getmtime(inputFile) # modificaiton time of the input file
-		self.outputModTime = os.path.getmtime(outputFile) # modification time of the output file
-		if self.inputModTime > self.outputModTime:
+		self.inputModTime = os.path.getmtime(inputFile) # modification time of the input file
+		if flag:
+			self.outputModTime = os.path.getmtime(outputFile) # modification time of the output file
+		else:
+			self.outputModTime = self.inputModTime - 1
+		if (self.inputModTime > self.outputModTime) or (not flag):
 			# output file is not up-to-date, recalculate outputFile from inputFile
-			print "Target file is not up-to-date. Updating..."
-			result = proc	 
-		return result
+			print "[STATUS] Target file (" + str(outputFile) + ") is not up-to-date. Updating..."
+		 	fun()
+		 	print "[STATUS] Done! "
+		 	return None
+		else:
+			print "[STATUS] Target file (" + str(outputFile) + ") is up-to-date." 
+			return None
 
-	
-	
+
+
