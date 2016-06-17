562f41c984d31127554b3ce9a0c9af32389bf60c
Xiaolin <xiaolinli2018@u.northwestern.edu>
2016-06-17 10:12:16 -0500

    first commit

---
+++ Python/fileDependency.py
@@ -0,0 +1,20 @@
+# This function compares the modification time of input file and output file
+# if the output file is not up-to-date, i.e. the modification time is earlier for output file
+# updating steps will be called.
+import os.path
+
+class fileDependency():
+	def __init__(self, inputFile, outputFile, *proc):
+		# inputFile: input File path
+		# outputFile: output File path
+		# *proc: if the outputfile is not up-to-date, the processed to be taken
+		
+		self.inputModTime = os.path.getmtime(inputFile) # modificaiton time of the input file
+		self.outputModTime = os.path.getmtime(outputFile) # modification time of the output file
+		if self.inputModTime > self.outputModTime:
+			# output file is not up-to-date, recalculate outputFile from inputFile
+			proc	 
+			
+
+	
+	
